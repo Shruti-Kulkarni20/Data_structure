84486c8a5456d65a59ec6fd3baa34c38f226b275
Shruti-Kulkarni20 <shrutikulkarni503@gmail.com>
2026-01-09 22:17:02 +0530

    Add files via upload

---
+++ C/ReverseLinkedList.c
@@ -0,0 +1,81 @@
+//Reverse Linked list //3 pointer algorithm
+
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct node
+{
+    int data;
+    struct node *next;
+}NODE,*PNODE,**PPNODE;
+
+void InsertLast(PPNODE first,int no)
+{
+    PNODE newn = NULL;
+    PNODE temp = NULL;
+    newn =(PNODE)malloc(sizeof(NODE));
+    newn->data = no;
+    newn->next = NULL;
+
+    if(*first == NULL)
+    {
+        *first = newn;
+    }
+    else
+    {
+        temp = *first;
+        while (temp->next != NULL)
+        {
+            temp = temp ->next;
+        }
+        temp -> next = newn; 
+    }
+}
+void Display(PNODE first)
+{
+    while (first != NULL)
+    {
+        printf("| %d |->",first->data);
+        first = first->next;
+    }
+    printf("NULL\n");
+}
+
+void ReverseLinkedList(PPNODE first)
+{
+    PNODE previous = NULL;
+    PNODE current = NULL;
+    PNODE next = NULL;
+
+    if(*first == NULL)
+    {
+        return;
+    }
+
+    current = *first;
+
+    while (current != NULL)
+    {
+        next = current->next;
+        current->next = previous;
+        previous = current;
+        current = next;
+    }
+    *first = previous;
+}
+
+int main()
+{
+    PNODE head = NULL;
+    InsertLast(&head,11);
+    InsertLast(&head,21);
+    InsertLast(&head,51);
+    InsertLast(&head,101);
+    InsertLast(&head,111);
+
+
+    ReverseLinkedList(&head);
+    Display(head);
+
+    return 0;
+}
